fc1d81db765b7f85d486615142c17cd00dc81778
Sherry Ma <sherry.hyma@gmail.com>
2018-09-03 10:43:24 +0800

    add onscroll navbar hide

--- js/customjs.js
+++ js/customjs.js
@@ -1,3 +1,24 @@
-function scrollloader() {
-	OnClick
-}
+function navControl() {
+	var navbar = document.getElementById("nav");
+	// var winheight = window.innerHeight || document.body.clientHeight
+	// var docheight = getDocMaxHeight()
+	// var scrollTop = window.pageYOffset || document.body.parentNode || document.body.scrollTop
+	// var trackLength = docheight - winheight
+	// var percentageScrolled = Math.floor(scrollTop/trackLength * 100)
+	percentageScrolled = 10
+	if (percentageScrolled > 20) {
+		console.log(percentageScrolled);
+	  navbar.addClass('navbar-gone');
+	} else {
+		console.log(hi);
+		console.log(percentageScrolled);
+		navbar.removeClass('navbar-gone');
+	}
+  }
+  
+//   function getDocMaxHeight() {
+// 	return Math.max(
+// 	  document.body.scrollHeight, document.body.offsetHeight,
+// 	  document.body.clientHeight
+// 	)
+//   }
